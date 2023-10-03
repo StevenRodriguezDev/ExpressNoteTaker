fee6fe02d73fc418ffd9a5f0bf9dcf10498fd4da
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-10-02 21:23:22 -0600

    created api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const fs = require("fs");
+
+
+// router.get('/notes', (req, res) =>
+// //api responible for reading existing note 
+// //   res.sendFile(path.join(__dirname, '../public/notes.html'))
+// );
+
+// //
+// router.post('/notes', (req, res) =>
+
+// //   res.sendFile(path.join(__dirname, '../public/index.html'))
+// );
+
+ module.exports = router;
