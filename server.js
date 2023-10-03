9e0dcc0791899c2f632bd51bc69455ae48d47dae
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-10-02 22:04:00 -0600

    updated server.js works!

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 //dependencies
 const express = require('express');
 const path = require('path');
-const uuid = require('.helpers/uuid');
-const fsUtils = require('.helpers/fsUtils');
+const uuid = require('./helpers/uuid');
+const fsUtils = require('./helpers/fsUtils');
 const dbDAta = require('./db/db.json')
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 //set up 
@@ -14,11 +12,39 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// app.use('/api',apiRoutes)
-app.use('/',htmlRoutes)
 
 
+//html routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+  });
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+
+//api routes
+app.get('/api/notes', (req, res) => {
+//api responible for reading existing note 
+  fsUtils.readFromFile('./db/db.json').then((data) => {
+    res.json(JSON.parse(data))
+  })
+});
+
+// //
+app.post('/api/notes', (req, res) => {
+const { title, text } = req.body;
+const userNote = {
+    title: title,
+    text: text,
+    id: uuid(),
+};
+
+fsUtils.readAndAppend(userNote, "./db/db.json");
+  console.log(userNote);
+  res.json("YAY Note is added!");
+});
 
 
 
